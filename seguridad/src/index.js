a5197044f932b6a8d2d0733fb63333e85fe76df7
Anghelo Cuba <anghelocuba2310@gmail.com>
2025-12-15 15:51:26 -0500

    creando modulo seguridad

--- seguridad/src/index.js
+++ seguridad/src/index.js
@@ -1,15 +1,12 @@
-// seguridad/src/index.js
 const Hapi = require('@hapi/hapi');
 const path = require('path');
-// Importa las rutas (las crearemos en el siguiente paso)
 const securityRoutes = require('./routes/security.routes'); 
-// Carga las variables de entorno si no usas el -r dotenv/config
 require('dotenv').config({ path: path.resolve(__dirname, '..', '..', '..', '.env') }); 
 
 const init = async () => {
     const server = Hapi.server({
         port: process.env.SECURITY_PORT || 3000,
-        host: '0.0.0.0', // Es bueno usar 0.0.0.0 para contenedores
+        host: '0.0.0.0', 
         routes: {
             cors: {
                 origin: ['http://localhost:4200'], 
@@ -24,8 +21,7 @@ const init = async () => {
     console.log(`Microservicio de Seguridad corriendo en: ${server.info.uri}`);
 };
 
-// Manejo de errores de inicio
-process.on('unhandledRejection', (err) => {
+process.on('handler', (err) => {
     console.log(err);
     process.exit(1);
 });
--- seguridad/src/services/token.service.js
+++ seguridad/src/services/token.service.js
@@ -1,23 +1,11 @@
-const db = {
-    // Simulación de guardar en una tabla de tokens (Token, Fecha, Validez)
-    saveToken: (token) => {
-        console.log(`[Seguridad] Token ${token} guardado en DB.`);
-        // En un entorno real: INSERT INTO tokens (token_value, created_at, is_valid) VALUES (...)
-        return true;
-    },
-    // Simulación de validación (el MS Clientes lo usa)
-    findToken: (token) => {
-        // En un entorno real: SELECT is_valid FROM tokens WHERE token_value = ?
-        return token && token.length === 8; // Simplemente verifica el formato por ahora
-    }
-};
+
 
 const generateToken = () => {
     // Genera un número aleatorio de 8 dígitos
     return Math.floor(10000000 + Math.random() * 90000000).toString();
 };
 
-const generateAndSaveToken = () => {
+/*const generateAndSaveToken = () => {
     const token = generateToken();
     db.saveToken(token);
     return token;
@@ -26,8 +14,8 @@ const generateAndSaveToken = () => {
 const validateToken = (token) => {
     return db.findToken(token);
 };
-
+*/
 module.exports = {
-    generateAndSaveToken,
-    validateToken
+    //generateAndSaveToken,
+    //validateToken
 };
